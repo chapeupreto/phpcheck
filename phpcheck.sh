8734f4e58c1a63b2b6bff2f3081127994ba80e88
Rodiney Elias <rodiney@fretebras.com.br>
2017-02-02 10:14:27 -0200

    first commit

---
+++ phpcheck.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+which php &> /dev/null
+if [[ $? -ne 0 ]]; then
+	echo -e "\nSorry! You need to have PHP installed in order for this to work.\n\n"
+	exit
+fi
+
+arg="${1:-.}"
+
+if [[ -f "${arg}" ]]; then
+	php -l "${arg}"
+fi
+
+if [[ -d "${arg}" ]]; then
+	UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+	find "${arg}" -name '*.php' -exec php -l \{\} > /dev/null 2> /tmp/"${UUID}" \;
+
+	if [[ -s /tmp/"${UUID}" ]]; then
+		cat /tmp/"${UUID}" && rm /tmp/"${UUID}" && exit 1;
+	fi
+
+	exit 0;
+fi
